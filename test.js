24327887dc0fa003af36f49a62087a374084a29c
Snuffish <snuffish90@gmail.com>
2024-11-13 03:58:58 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,30 @@
+console.log("HEJSAN")
+
+const testar = 'testar'
+test
+console.log(testar)
+test
+			testar = 'hejsan'
+
+console.log(testar)
+
+
+123456789123456789123456789123456789123456789123456789
+123456789123456789123456789123456789123456789123456789
+123456789123456789123456789123456789123456789123456789
+123456789123456789123456789123456789123456789123456789
+123456789123456789123456789123456789123456789123456789
+// testar lite hä
+//
+//// hgfd hgfd hgfd hgfdkm hnsfgd // hgfd hgfd hgfd hgfdkm hnsfgd // hgfd hgfd hgfd hgfdkm hnsfgd // hgfd hgfd hgfd hgfdkm hnsfgd 
+// r
+//dsafdsf dsjfk hdsjkf ghdsjk gfds g
+// hgfdhgfd
+// hgfd hgfd hgfd hgfdkm hnsfgd // hgfd hgfd hgfd hgfdkm hnsfgd // hgfd hgfd hgfd hgfdkm hnsfgd 
+/// hgfd hgfd hgfd hgfdkm hnsfgd // hgfd hgfd hgfd hgfdkm hnsfgd // h// hgfd hgfd hgfd hgfdkm hnsfgd // hgfd hgfd hgfd hgfdkm hnsfgd gfd hgfd hgfd hgfdkm hnsfgd / fds
+//gfdsg fdsfd g// hgfd hgfd hgfd hgfdkm hnsfgd 
+//g fdsg fdsgfd sgfdhs gjksfdh gfds g// hgfd hgfd hgfd hgfdkm hnsfgd 
+//// hgfd hgfd hgfd hgfdkm hnsfgd // hgfd hgfd hgfd hgfdkm hnsfgd 
+// dsadfds// hgfd hgfd hgfd hgfdkm hnsfgd // hgfd hgfd hgfd hgfdkm hnsfgd // hgfd hgfd hgfd hgfdkm hnsfgd 
+
+
