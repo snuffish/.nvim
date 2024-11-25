098d8cbec742b8a76b26aae35e70fbcdbbd631f5
Snuffish <snuffish90@gmail.com>
2024-11-25 13:57:54 +0100

    wip

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,17 @@
+vim.keymap.set('n', '<leader>nd', '<cmd>NoiceDismiss<CR>', { desc = 'Dismiss Noice Message' })
+
+return {
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    'MunifTanjim/nui.nvim',
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    'rcarriga/nvim-notify',
+  },
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,60 +6,59 @@
 --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 
 return {
-    'nvim-treesitter/nvim-treesitter',
-    event = {'BufReadPre', 'BufNewFile'},
-    build = ':TSUpdate',
-    dependencies = {'windwp/nvim-ts-autotag'},
-    config = function()
-        -- import nvim-treesitter plugin
-        local treesitter = require 'nvim-treesitter.configs'
-
-        -- configure treesitter
-        treesitter.setup { -- enable syntax highlighting
-            highlight = {
-                enable = true
-            },
-            -- enable indentation
-            indent = {
-                enable = true
-            },
-            -- enable autotagging (w/ nvim-ts-autotag plugin)
-            autotag = {
-                enable = true
-            },
-            -- ensure these language parsers are installed
-            ensure_installed = {
-              'bash',
-              'c',
-              'diff',
-              'html',
-              'lua',
-              'luadoc',
-              'markdown',
-              'markdown_inline',
-              'query',
-              'vim',
-              'vimdoc',
-              'javascript',
-              'typescript',
-              'python',
-              'rust',
-              'go',
-              'cpp',
-              'css',
-              'json',
-              'yaml',
-              'toml',
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = '<C-space>',
-                    node_incremental = '<C-space>',
-                    scope_incremental = false,
-                    node_decremental = '<bs>'
-                }
-            }
-        }
-    end
+  'nvim-treesitter/nvim-treesitter',
+  event = { 'BufReadPre', 'BufNewFile' },
+  build = ':TSUpdate',
+  dependencies = { 'windwp/nvim-ts-autotag' },
+  config = function()
+    -- import nvim-treesitter plugin
+    local treesitter = require 'nvim-treesitter.configs'
+    -- configure treesitter
+    treesitter.setup { -- enable syntax highlighting
+      highlight = {
+        enable = true,
+      },
+      -- enable indentation
+      indent = {
+        enable = true,
+      },
+      -- enable autotagging (w/ nvim-ts-autotag plugin)
+      autotag = {
+        enable = true,
+      },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'javascript',
+        'typescript',
+        'python',
+        'rust',
+        'go',
+        'cpp',
+        'css',
+        'json',
+        'yaml',
+        'toml',
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-space>',
+          node_incremental = '<C-space>',
+          scope_incremental = false,
+          node_decremental = '<bs>',
+        },
+      },
+    }
+  end,
 }
