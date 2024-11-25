58e80609ffaf4cfe697ffe8bbb47237377dbb92d
Snuffish <snuffish90@gmail.com>
2024-11-25 16:27:47 +0100

    wip

--- lua/plugins/easymotion.lua
+++ lua/plugins/easymotion.lua
@@ -1,9 +1,10 @@
 -- https://github.com/easymotion/vim-easymotion
 local utils = require '../utils'
+local set = utils.set
 
-vim.api.nvim_set_keymap('n', '<Leader><Leader>s', '<Plug>(easymotion-overwin-f)', {})
+set('n', 'f', '<Plug>(easymotion-overwin-f)', { noremap = true, desc = 'Search for word' })
+
 vim.api.nvim_set_keymap('n', '<Leader><Leader>q', '<Plug>(easymotion-overwin-line)', {})
-utils.set('n', '<leader>w', '<Plug>(easymotion-2)', { noremap = true, desc = 'Search for word' })
 
 return {
   'easymotion/vim-easymotion',
