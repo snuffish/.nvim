69814a5b70ec3545a7660dc9b47e383c747d5ded
Christoffer Engman <x-chen@oresundsbron.com>
2024-11-26 10:59:38 +0100

    wip

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,17 +1,13 @@
-vim.keymap.set('n', '<leader>nd', '<cmd>NoiceDismiss<CR>', { desc = 'Dismiss Noice Message' })
-
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
-  opts = {
-    -- add any options here
-  },
+  opts = {},
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     'MunifTanjim/nui.nvim',
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
     'rcarriga/nvim-notify',
   },
+  keys = function()
+    vim.keymap.set('n', '<leader>nd', '<cmd>NoiceDismiss<CR>', { desc = 'Dismiss Noice Message' })
+    vim.keymap.set('n', '<leader>nt', '<cmd>Telescope noice<CR>', { desc = 'Show telescope noice' })
+  end,
 }
