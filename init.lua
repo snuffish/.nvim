466ef15cac6fd54bc014f96bcab2e280ec2c3077
Snuffish <snuffish90@gmail.com>
2024-11-26 10:49:57 +0100

    wip

--- init.lua
+++ init.lua
@@ -456,6 +456,7 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'plugins' },
   { import = 'config' },
+  { import = 'games' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
